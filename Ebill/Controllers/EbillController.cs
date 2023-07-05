6b4f9df01ba456921facbba9b0a235a952ef1d01
Nbvuong2k1 <vuongspectre201@gmail.com>
2023-07-05 09:54:08 +0700

    Add project files.

---
+++ Ebill/Controllers/EbillController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ebill.Models;
+using Ebill.Repository;
+
+namespace Ebill.Controllers
+{
+    public class EbillController : Controller
+    {
+        // GET: Ebill
+        public ActionResult Index()
+        {
+            Data data= new Data();
+            var list = data.GetAllDetail();
+            return View(list);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(BillDetail details)
+        {
+            Data data = new Data();
+            data.SaveBillDetails(details);
+            ModelState.Clear();
+            return View();
+        }
+        public ActionResult CreateItem(Items item)
+        {
+
+            return PartialView("_CreateItem", item);
+        }
+        public ActionResult ViewBill(int Id)
+        {
+            Data data = new Data();
+            var details = data.GetDetail(Id);
+            return View(details);
+        }
+    }
+}
+//details
---
+++ Ebill/Repository/IData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ebill.Models;
+using System.Data.SqlClient;
+
+namespace Ebill.Repository
+{
+    internal interface IData
+    {
+        void SaveBillDetails(BillDetail details);
+        void SaveBillItems(List<Items> items, SqlConnection con, int id);
+    }
+}
